4d34adff37972f9721d4886dea2fed362bdfe90c
Soraahn <nikanordesin@gmail.com>
2026-02-06 19:42:27 +0300

    feat(services): complete section layout and decorations

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -73,10 +73,37 @@
 <style scoped>
   .services {
     position: relative;
+    padding-top: 96px;
+    overflow-x: clip;
   }
-  .services__intro   {
+  .services::before {
+    content: "";
+    position: absolute;
+    left: 0;
+    top: -1px;
+    width: 100%;
+    height: 1126px;
+    background: #06255B;
+    z-index: 0;
+  }
+  .services::after {
+    content: "";
+    position: absolute;
+    z-index: 0;
+    width: 2500px;
+    height: 2500px;
+    background: #ffffff;
+    top: 1125px;
+
+    transform: rotate(-45deg);
+    transform-origin: top left;
+  }
+  .services > * {
+    position: relative;
+    z-index: 1;
+  }
+  .services__intro {
     margin-left: 287px;
-    margin-top: 96px;
     max-width: fit-content;
     color: white;
     background-color: #06255B;
@@ -97,11 +124,24 @@
     letter-spacing: 0.3px;
   }
   .services__viewport {
-    overflow: hidden;
+    position: relative;
     margin-top: -360px;
     max-width: 1440px;
     transform: translateX(25px);
     padding: 246px 0 17px 0;
+  }
+  .services__viewport::after {
+    content: '';
+    display: block;
+    position: absolute;
+    right: -50px;
+    top: 760px;
+    width: 727px;
+    height: 515px;
+    background: url("@/assets/services/png/building.png") no-repeat;
+    background-size: contain;
+    background-position: center;
+    z-index: -1;
   }
   .services__list {
     display: grid;
@@ -152,6 +192,17 @@
   }
   .services__item[data-area="h"] {
     grid-area: h;
+    position: relative;
+    z-index: 1;
+  }
+  .services__item[data-area="g"]::before {
+    content: '';
+    position: absolute;
+    width: 800px;
+    height: 800px;
+    background: #F7F9FC;
+    left: 0;
+    top: 0;
   }
   .services__item-viewport {
     transform: rotate(45deg);
@@ -186,9 +237,9 @@
     transform: translateX(0px) translateY(0px);
   }
   .services__scroll-hint {
+    margin-top: 135px;
     display: flex;
     justify-content: center;
-    margin-top: 39px;
   }
   .scroll-hint__text {
     display: flex;
